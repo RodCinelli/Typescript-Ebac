22b820912c77ddce49e1b6872d38a94780801a53
Rodrigo Cinelli <rodcinelli@gmail.com>
2024-05-31 02:14:00 -0300

    Configuranto o typescript

---
+++ src/tipos_basicos.ts
@@ -0,0 +1,24 @@
+let estaChovendo: boolean = false;
+
+estaChovendo = true;
+
+let idade: number = 35;
+
+let altura: number = 1.75;
+
+const nacionalidade: string = "brasileiro";
+
+const colegas: string[] = ["Rodrigo", "Guilherme", "João"];
+const tecnologias: Array<string> = ["JavaScript", "TypeScript", "Node.js"];
+const notas: ReadonlyArray<number> = [10, 9, 8, 7, 6];
+
+const lista: [nome: string, estaestudando: boolean, idade: number] = ["Rodrigo", true, 35];
+
+let idadeDaAna: number | string  = 25;
+idadeDaAna = "25 anos";
+
+let dadosDaApi: any;
+dadosDaApi = 10;
+dadosDaApi = true;
+dadosDaApi = [1, 2, 3];
+dadosDaApi = "string";
