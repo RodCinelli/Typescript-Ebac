45b963fff1ef50bb66ded05084bb445731c968d1
Rodrigo Cinelli <rodcinelli@gmail.com>
2024-05-31 02:54:33 -0300

    Alterando aspas duplas para aspas simples

--- src/teste.ts
+++ src/teste.ts
@@ -1,1 +1,1 @@
-let none = "rodrigo"
+let none = 'rodrigo';
--- src/tipos_basicos.ts
+++ src/tipos_basicos.ts
@@ -1,24 +1,24 @@
-let estaChovendo: boolean = false;
+let estaChovendo = false;
 
 estaChovendo = true;
 
-let idade: number = 35;
+let idade = 35;
 
-let altura: number = 1.75;
+let altura = 1.75;
 
-const nacionalidade: string = "brasileiro";
+const nacionalidade = 'brasileiro';
 
-const colegas: string[] = ["Rodrigo", "Guilherme", "João"];
-const tecnologias: Array<string> = ["JavaScript", "TypeScript", "Node.js"];
-const notas: ReadonlyArray<number> = [10, 9, 8, 7, 6];
+const colegas = ['Rodrigo', 'Guilherme', 'João'];
+const tecnologias = ['JavaScript', 'TypeScript', 'Node.js'];
+const notas = [10, 9, 8, 7, 6];
 
-const lista: [nome: string, estaestudando: boolean, idade: number] = ["Rodrigo", true, 35];
+const lista = ['Rodrigo', true, 35];
 
 let idadeDaAna: number | string  = 25;
-idadeDaAna = "25 anos";
+idadeDaAna = '25 anos';
 
-let dadosDaApi: any;
+let dadosDaApi;
 dadosDaApi = 10;
 dadosDaApi = true;
 dadosDaApi = [1, 2, 3];
-dadosDaApi = "string";
+dadosDaApi = 'string';
